bb4dfc44631266ebf10a9f5c37118c521e562f51
sdhtele <97067334+sdhtele@users.noreply.github.com>
2024-03-12 01:46:09 +0700

    comment

--- bot/helper/mirror_utils/status_utils/mega_download_status.py
+++ bot/helper/mirror_utils/status_utils/mega_download_status.py
@@ -1,61 +1,80 @@
 from bot.helper.ext_utils.bot_utils import get_readable_file_size,MirrorStatus, get_readable_time
 from bot import DOWNLOAD_DIR
 
-
 class MegaDownloadStatus:
+    """
+    A class representing the status of a Mega download.
+    """
 
     def __init__(self, obj, listener):
-        self.__uid = obj.uid
-        self.__listener = listener
-        self.__obj = obj
-        self.message = listener.message
+        """
+        Initialize a new MegaDownloadStatus object.
+
+        :param obj: The download object to monitor.
+        :type obj: object
+        :param listener: The listener object to use for sending messages.
+        :type listener: object
+        """
+        self.__uid = obj.uid  # The unique identifier for the download.
+        self.__listener = listener  # The listener object for sending messages.
+        self.__obj = obj  # The download object to monitor.
+        self.message = listener.message  # The message object to send responses to.
 
     def name(self) -> str:
+        """
+        Get the name of the download.
+
+        :return: The name of the download.
+        :rtype: str
+        """
         return self.__obj.name
 
     def progress_raw(self):
+        """
+        Calculate the progress of the download in raw format (as a percentage).
+
+        :return: The progress of the download as a percentage.
+        :rtype: float
+        """
         try:
             return round(self.processed_bytes() / self.__obj.size * 100,2)
         except ZeroDivisionError:
             return 0.0
 
     def progress(self):
-        """Progress of download in percentage"""
+        """
+        Get the progress of the download as a formatted string.
+
+        :return: The progress of the download as a formatted string.
+        :rtype: str
+        """
         return f"{self.progress_raw()}%"
 
     def status(self) -> str:
+        """
+        Get the current status of the download.
+
+        :return: The current status of the download.
+        :rtype: str
+        """
         return MirrorStatus.STATUS_DOWNLOADING
 
     def processed_bytes(self):
+        """
+        Get the number of bytes that have been processed by the download.
+
+        :return: The number of processed bytes.
+        :rtype: int
+        """
         return self.__obj.downloaded_bytes
 
     def eta(self):
+        """
+        Calculate the estimated time of arrival for the download.
+
+        :return: The estimated time of arrival as a formatted string.
+        :rtype: str
+        """
         try:
             seconds = (self.size_raw() - self.processed_bytes()) / self.speed_raw()
-            return f'{get_readable_time(seconds)}'
-        except ZeroDivisionError:
-            return '-'
-
-    def size_raw(self):
-        return self.__obj.size
-
-    def size(self) -> str:
-        return get_readable_file_size(self.size_raw())
-
-    def downloaded(self) -> str:
-        return get_readable_file_size(self.__obj.downloadedBytes)
-
-    def speed_raw(self):
-        return self.__obj.speed
-
-    def speed(self) -> str:
-        return f'{get_readable_file_size(self.speed_raw())}/s'
-
-    def gid(self) -> str:
-        return self.__obj.gid
-
-    def path(self) -> str:
-        return f"{DOWNLOAD_DIR}{self.__uid}"
-
-    def download(self):
-        return self.__obj
+            return f'{
