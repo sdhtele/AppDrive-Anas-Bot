79a6b70f5b1ab3a377bc88b52437ca524494eaed
sdhtele <97067334+sdhtele@users.noreply.github.com>
2024-03-12 01:46:05 +0700

    comment

--- generate_string_session.py
+++ generate_string_session.py
@@ -1,6 +1,10 @@
-from pyrogram import Client
+from pyrogram import Client # Import the Client class from the pyrogram library
 
+# Define the API key and hash as variables
 API_KEY = int(input("Enter API KEY: "))
 API_HASH = input("Enter API HASH: ")
+
+# Create a new in-memory Client instance with the specified API key and hash
 with Client(':memory:', api_id=API_KEY, api_hash=API_HASH) as app:
+    # Export the session string for the Client instance
     print(app.export_session_string())
