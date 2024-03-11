6d6cbd26065d26eef18e2aec67000d3b74c229a9
sdhtele <97067334+sdhtele@users.noreply.github.com>
2024-03-12 01:46:14 +0700

    comment

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -1,63 +1,36 @@
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler  # Importing CommandHandler from telegram.ext
 
-from bot import dispatcher
-from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.telegram_helper.message_utils import deleteMessage, sendMessage
-from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot.helper.ext_utils.bot_utils import is_gdrive_link, is_gdtot_link, new_thread, appdrive_dl
-from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot
-from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
+from bot import dispatcher  # Importing dispatcher from bot.helper.mirror_utils.upload_utils.gdriveTools
+from bot.helper.telegram_helper.message_utils import deleteMessage, sendMessage  # Importing sendMessage and deleteMessage from bot.helper.telegram_helper.message_utils
+from bot.helper.telegram_helper.filters import CustomFilters  # Importing CustomFilters from bot.helper.telegram_helper.filters
+from bot.helper.telegram_helper.bot_commands import BotCommands  # Importing BotCommands from bot.helper.telegram_helper.bot_commands
+from bot.helper.ext_utils.bot_utils import is_gdrive_link, is_gdtot_link, new_thread, appdrive_dl  # Importing is_gdrive_link, is_gdtot_link, new_thread, and appdrive_dl from bot.helper.ext_utils.bot_utils
+from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot  # Importing gdtot from bot.helper.mirror_utils.download_utils.direct_link_generator
+from bot.helper.ext_utils.exceptions import DirectDownloadLinkException  # Importing DirectDownloadLinkException from bot.helper.ext_utils.exceptions
 
-@new_thread
+@new_thread  # Decorator to run the function in a new thread
 def countNode(update, context):
-    args = update.message.text.split(" ", maxsplit=1)
-    reply_to = update.message.reply_to_message
-    link = ''
-    if len(args) > 1:
-        link = args[1]
-        if update.message.from_user.username:
-            tag = f"@{update.message.from_user.username}"
+    args = update.message.text.split(" ", maxsplit=1)  # Splitting the user's message into arguments
+    reply_to = update.message.reply_to_message  # Getting the message that the user replied to
+    link = ''  # Initializing the link variable
+    if len(args) > 1:  # If there is more than one argument
+        link = args[1]  # The second argument is the link
+        if update.message.from_user.username:  # If the user has a username
+            tag = f"@{update.message.from_user.username}"  # The tag is the username
         else:
-            tag = update.message.from_user.mention_html(update.message.from_user.first_name)
-    elif reply_to is not None:
-        if len(link) ==0:
-            link = reply_to.text
-        if reply_to.from_user.username:
-            tag = f"@{reply_to.from_user.username}"
+            tag = update.message.from_user.mention_html(update.message.from_user.first_name)  # Otherwise, the tag is the user's first name
+    elif reply_to is not None:  # If the user replied to a message
+        if len(link) ==0:  # If the link is not provided in the command
+            link = reply_to.text  # The link is the replied message
+        if reply_to.from_user.username:  # If the user who sent the replied message has a username
+            tag = f"@{reply_to.from_user.username}"  # The tag is the username
         else:
-            tag = reply_to.from_user.mention_html(reply_to.from_user.first_name)
-    gdtot_link = is_gdtot_link(link)
-    if 'driveapp.in' in link:
+            tag = reply_to.from_user.mention_html(reply_to.from_user.first_name)  # Otherwise, the tag is the user's first name
+    gdtot_link = is_gdtot_link(link)  # Checking if the link is a Google Takeout link
+    if 'driveapp.in' in link:  # If the link is an App Drive link
        is_driveapp = True
-    if 'appdrive.in' in link:
+    if 'appdrive.in' in link:  # If the link is an App Drive link
        is_appdrive = True
-    if is_driveapp:
+    if is_driveapp:  # If the link is an App Drive link
         try:
-            link = appdrive_dl(link)
-        except DirectDownloadLinkException as e:
-            return sendMessage(str(e), context.bot, update)
-    if is_appdrive:
-        try:
-            link = appdrive_dl(link)
-        except DirectDownloadLinkException as e:
-            return sendMessage(str(e), context.bot, update)
-    if gdtot_link:
-        try:
-            link = gdtot(link)
-        except DirectDownloadLinkException as e:
-            return sendMessage(str(e), context.bot, update)
-    if is_gdrive_link(link):
-        msg = sendMessage(f"Counting: <code>{link}</code>", context.bot, update)
-        gd = GoogleDriveHelper()
-        result = gd.count(link)
-        deleteMessage(context.bot, msg)
-        cc = f'\n\n<b>cc: </b>{tag}'
-        sendMessage(result + cc, context.bot, update)
-        if gdtot_link:
-            gd.deletefile(link)
-    else:
-        sendMessage('Send Gdrive link along with command or by replying to the link by command', context.bot, update)
-
-count_handler = CommandHandler(BotCommands.CountCommand, countNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-dispatcher.add_handler(count_handler)
+            link = app
