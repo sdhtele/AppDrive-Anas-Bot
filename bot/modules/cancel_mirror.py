cb935e3b04cbc139d5456596861b033cd7860c66
sdhtele <97067334+sdhtele@users.noreply.github.com>
2024-03-12 01:46:12 +0700

    comment

--- bot/modules/cancel_mirror.py
+++ bot/modules/cancel_mirror.py
@@ -1,15 +1,15 @@
 from telegram.ext import CommandHandler
-from time import sleep
+from time import sleep  # Importing sleep from time module
 
 from bot import download_dict, dispatcher, download_dict_lock, DOWNLOAD_DIR
-from bot.helper.ext_utils.fs_utils import clean_download
-from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.telegram_helper.message_utils import sendMessage
-from bot.helper.ext_utils.bot_utils import getDownloadByGid, MirrorStatus, getAllDownload
-
+from bot.helper.ext_utils.fs_utils import clean_download  # Importing clean_download from fs_utils
+from bot.helper.telegram_helper.bot_commands import BotCommands  # Importing BotCommands
+from bot.helper.telegram_helper.filters import CustomFilters  # Importing CustomFilters
+from bot.helper.telegram_helper.message_utils import sendMessage  # Importing sendMessage
+from bot.helper.ext_utils.bot_utils import getDownloadByGid, MirrorStatus, getAllDownload  # Importing required functions from bot_utils
 
 def cancel_mirror(update, context):
+    """Cancels the mirror download with the given GID or the replied message's download."""
     args = update.message.text.split(" ", maxsplit=1)
     mirror_message = None
     if len(args) > 1:
@@ -43,6 +43,7 @@ def cancel_mirror(update, context):
         dl.download().cancel_download()
 
 def cancel_all(update, context):
+    """Cancels all the downloads."""
     count = 0
     gid = 0
     while True:
@@ -57,11 +58,10 @@ def cancel_all(update, context):
             break
     sendMessage(f'{count} Download(s) has been Cancelled!', context.bot, update)
 
-
-
 cancel_mirror_handler = CommandHandler(BotCommands.CancelMirror, cancel_mirror,
                                        filters=(CustomFilters.authorized_chat | CustomFilters.authorized_user) & CustomFilters.mirror_owner_filter | CustomFilters.sudo_user, run_async=True)
 cancel_all_handler = CommandHandler(BotCommands.CancelAllCommand, cancel_all,
                                     filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
 dispatcher.add_handler(cancel_all_handler)
 dispatcher.add_handler(cancel_mirror_handler)
+
