d809d42759643ba39f5c3e95eabe77035619b10e
sdhtele <97067334+sdhtele@users.noreply.github.com>
2024-03-12 01:46:06 +0700

    comment

--- bot/modules/__init__.py
+++ bot/modules/__init__.py
@@ -1,1 +1,26 @@
+def add_numbers(num1, num2):
+    """
+    This function takes in two numbers as arguments and returns their sum.
+    """
+    return num1 + num2
 
+result = add_numbers(3, 5)
+print(result)
+
+
+# This is a function definition for a function named 'add_numbers'
+# The function takes in two arguments: 'num1' and 'num2'
+# Both 'num1' and 'num2' are expected to be numerical values
+def add_numbers(num1, num2):
+    """
+    This function takes in two numbers as arguments and returns their sum.
+    """
+    # The 'return' statement ends the function and sends a value back to the caller
+    # In this case, the function returns the sum of 'num1' and 'num2'
+    return num1 + num2
+
+# Call the 'add_numbers' function with arguments 3 and 5
+result = add_numbers(3, 5)
+
+# Print the result of the function call
+print(result)
