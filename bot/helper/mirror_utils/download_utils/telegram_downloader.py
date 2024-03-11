44dac312730c7255c1840bfdad3159aec77b78bc
sdhtele <97067334+sdhtele@users.noreply.github.com>
2024-03-12 01:46:15 +0700

    comment

--- bot/helper/mirror_utils/download_utils/telegram_downloader.py
+++ bot/helper/mirror_utils/download_utils/telegram_downloader.py
@@ -1,7 +1,6 @@
 import logging
 import random
-
-from time import time
+import time
 from threading import RLock, Lock, Thread
 
 from bot import LOGGER, download_dict, download_dict_lock, app, STOP_DUPLICATE
@@ -9,13 +8,19 @@ from ..status_utils.telegram_download_status import TelegramDownloadStatus
 from bot.helper.telegram_helper.message_utils import sendMarkup, sendStatusMessage
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 
+# Global lock to ensure only one thread can modify the GLOBAL_GID set at a time
 global_lock = Lock()
+# Set to store the IDs of files currently being downloaded
 GLOBAL_GID = set()
+
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 class TelegramDownloadHelper:
     def __init__(self, listener):
+        """
+        Initialize the TelegramDownloadHelper class with a listener object.
+        """
         self.name = ""
         self.size = 0
         self.progress = 0
@@ -28,10 +33,16 @@ class TelegramDownloadHelper:
 
     @property
     def download_speed(self):
+        """
+        Calculate and return the download speed in bytes per second.
+        """
         with self.__resource_lock:
             return self.downloaded_bytes / (time() - self.__start_time)
 
     def __onDownloadStart(self, name, size, file_id):
+        """
+        Callback function called when a download starts.
+        """
         with global_lock:
             GLOBAL_GID.add(file_id)
         with self.__resource_lock:
@@ -44,6 +55,9 @@ class TelegramDownloadHelper:
         sendStatusMessage(self.__listener.update, self.__listener.bot)
 
     def __onDownloadProgress(self, current, total):
+        """
+        Callback function called periodically with the current and total number of bytes downloaded.
+        """
         if self.__is_cancelled:
             self.__onDownloadError('Cancelled by user!')
             app.stop_transmission()
@@ -56,6 +70,9 @@ class TelegramDownloadHelper:
                 pass
 
     def __onDownloadError(self, error):
+        """
+        Callback function called when an error occurs during the download.
+        """
         with global_lock:
             try:
                 GLOBAL_GID.remove(self.__id)
@@ -64,11 +81,17 @@ class TelegramDownloadHelper:
         self.__listener.onDownloadError(error)
 
     def __onDownloadComplete(self):
+        """
+        Callback function called when the download is complete.
+        """
         with global_lock:
             GLOBAL_GID.remove(self.__id)
         self.__listener.onDownloadComplete()
 
     def __download(self, message, path):
+        """
+        Download the media file from the provided message object.
+        """
         try:
             download = app.download_media(message,
                                                 progress = self.__onDownloadProgress,
@@ -83,6 +106,9 @@ class TelegramDownloadHelper:
             self.__onDownloadError('Internal error occurred')
 
     def add_download(self, message, path, filename):
+        """
+        Start a new download with the provided message, path, and filename.
+        """
         _message = app.get_messages(message.chat.id, reply_to_message_ids=message.message_id)
         media = None
         media_array = [_message.document, _message.video, _message.audio]
@@ -116,5 +142,8 @@ class TelegramDownloadHelper:
             self.__onDownloadError('No document in the replied message')
 
     def cancel_download(self):
+        """
+        Cancel the current download.
+        """
         LOGGER.info(f'Cancelling download on user request: {self.__id}')
         self.__is_cancelled = True
