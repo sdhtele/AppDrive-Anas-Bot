a6636c90958654b23190689455cc194716a3e22f
sdhtele <97067334+sdhtele@users.noreply.github.com>
2024-03-12 01:46:05 +0700

    comment

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-python3 update.py && python3 -m bot
+# Run the update script to ensure that the code is up-to-date.
+# This line executes the update.py script using Python 3.
+python3 update.py 
+
+# Once the code is updated, this line starts the bot using Python 3 and the -m flag.
+# The -m flag allows you to run a module as a script, and in this case, the "bot" module is being executed.
+python3 -m bot
